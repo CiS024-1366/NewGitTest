6bbd5e5233f57dd4b569a7bc370e8d4228a301da
CiS024-1366 <npadilla9982@gmail.com>
2021-09-15 15:22:26 -0700

    Create HelloWorld.java

---
+++ MyHomeWorkFiles/HelloWorld.java
@@ -0,0 +1,9 @@
+package MyHomeWorkFiles;
+
+public class HelloWorld   {  
+   
+    public static void main(final String[] args) {               
+             System.out.println("Hello world");    
+            
+    }
+}
